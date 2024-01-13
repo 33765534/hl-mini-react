b878c156ca1c496c40e7a07366ce60bbbe980328
lihuiling <337655347@qq.com>
2024-01-13 09:00:52 +0800

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-## 实现最简单的mini-react
-react API 的引用
-```
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-```
